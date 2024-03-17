4644db7642dc1bcbbffdd4c5ba48a103789f91a0
Navneet Singh <navneetsingh1825@gmail.com>
2024-03-17 19:19:57 +0530

    Adding some more lines

--- ARRAYS/Array Basics Questions/PairSum.java
+++ ARRAYS/Array Basics Questions/PairSum.java
@@ -2,10 +2,19 @@ public class PairSum {
     public static void main(String args[]){
         int arr[] = new int[]{1,2,3};
         int sum = 0;
-        for(int i = 0;i<arr.length;i++){
-            for(int j = 0;j<arr.length;j++){
-                sum = arr[i]+arr[j];
-                System.out.println("Sum of pairs ("+arr[i]+","+arr[j]+") is "+sum);
+        // for(int i = 0;i<arr.length;i++){
+        //     for(int j = 0;j<arr.length;j++){
+        //         sum = arr[i]+arr[j];
+        //         System.out.println("Sum of pairs ("+arr[i]+","+arr[j]+") is "+sum);
+        //     }
+        // }
+
+        // distinct sum
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                sum = arr[i] + arr[j];
+                System.out.println("Sum of pair (" + arr[i] + ", " + arr[j] + ") = " + sum);
             }
         }
     }
