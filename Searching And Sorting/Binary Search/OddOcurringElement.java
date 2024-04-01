e6396d2c32e7cc8d394944115970f5a4f8be929a
Navneet Singh <navneetsingh1825@gmail.com>
2024-04-01 22:20:11 +0530

    Initial commit

--- Searching And Sorting/Binary Search/OddOcurringElement.java
+++ Searching And Sorting/Binary Search/OddOcurringElement.java
@@ -1,8 +1,16 @@
 /*
-find odd occurring element in which 
-1. all element occur even no of time except one
-2. one element occur more than two at a time
-3. All pairs are repeat and not repeat
+L.C: 540. Single Element in a Sorted Array
+
+You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once.
+Return the single element that appears only once.
+Example 1:
+
+Input: nums = [1,1,2,3,3,4,4,8,8]
+Output: 2
+Example 2:
+
+Input: nums = [3,3,7,7,10,11,11]
+Output: 10
 */
 // Time complexity: O(logn)
 
