96c21349b17844ab7f71d40a3956a3b954961ccf
Navneet Singh <navneetsingh1825@gmail.com>
2024-04-17 00:54:20 +0530

    Initial commit

---
+++ Strings/Questions/ValidAnagram.java
@@ -0,0 +1,60 @@
+/*
+L.c 242. Valid Anagram
+
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+ */
+// 1st Appraoch : sort and compare
+// Time Complexity: O(nlogn)
+
+// import java.util.Arrays;
+// public class Anagram {
+    // public static boolean isAnagram(String s,String t){
+    //     char[] string1 = s.toCharArray();
+    //     char[] string2 = t.toCharArray();
+    //     Arrays.sort(string1);
+    //     Arrays.sort(string2);
+    //     return Arrays.equals(string1,string2);
+    // }
+
+
+    //  2nd Appraoch : using counting
+    // Time Complexity: O(n+m)
+    public class ValidAnagram {
+    public static boolean isAnagram(String s,String t){
+        int freqTable[] = new int[256];
+        for(int i = 0;i<s.length();i++){
+            freqTable[s.charAt(i)]++;
+        }
+        for(int i = 0;i<t.length();i++){
+            freqTable[t.charAt(i)]--;
+        }
+        for(int i = 0;i<256;i++){
+            if(freqTable[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String args[]){
+        String s = "anagram";
+        String t = "nagaram";
+        boolean result = isAnagram(s, t);
+        if(result){
+            System.out.println("Strings are Anagram");
+        }
+        else{
+            System.out.println("String are not Anagram");
+        }
+    }
+}
