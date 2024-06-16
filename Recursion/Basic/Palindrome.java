3cca43326a3937f6b1d3f1643dfe2cc7970d5f86
Navneet Singh <navneetsingh1825@gmail.com>
2024-06-16 16:34:03 +0530

    Initial commit

---
+++ Recursion/Basic/Palindrome.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Palindrome {
+    static int temp = 0;
+    public static void reverse(int n){
+        if(n==0){
+            return;
+        }
+        int rem = n%10;
+        temp =  temp*10+rem;
+        reverse(n/10);
+    }
+    public static boolean checkPalindrome(int n){
+        if(n == temp){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = s.nextInt();
+        reverse(n);
+        boolean ans = checkPalindrome(n);
+        if(ans){
+            System.out.println(n+ " is a palindromic number");
+        }
+        else{
+            System.out.println(n+" is not a palindromic number");
+        }
+        s.close();
+    }
+}
