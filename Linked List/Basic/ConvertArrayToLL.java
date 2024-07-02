c2e8ce9ab7d2ca84774541d9e688835eab950755
Navneet Singh <navneetsingh1825@gmail.com>
2024-07-02 22:21:52 +0530

    Initial commit

---
+++ Linked List/Basic/ConvertArrayToLL.java
@@ -0,0 +1,25 @@
+class Node{
+    Node next;
+    int data;
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+public class ConvertArrayToLL {
+    public static Node convertToLL(int arr[]){
+        Node head = new Node(arr[0]);
+        Node mover = head;
+        for(int i = 1;i<arr.length;i++){
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+        return head;
+    }
+    public static void main(String args[]){
+        int arr[] = new int[]{12,4,5,7,8};
+        Node head = convertToLL(arr);
+        System.out.println("First element is "+head.data);
+    }
+}
