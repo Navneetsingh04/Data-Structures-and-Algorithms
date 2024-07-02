76f57b05c6a2a9c2021c5eb12d96700132dc14ff
Navneet Singh <navneetsingh1825@gmail.com>
2024-07-02 22:21:32 +0530

    Initial commit

---
+++ Linked List/Basic/Checkelement.java
@@ -0,0 +1,31 @@
+class Node{
+    Node next;
+    int data;
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+public class Checkelement {
+    public static boolean checkifpresent(Node head,int element){
+        Node temp = head;
+        while(temp != null){
+            if(temp.data == element){
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
+    public static void main(String args[]){
+        int arr[] = new int[]{12,4,5,7,8};
+        Node head = new Node(arr[0]);
+        boolean ans = checkifpresent(head, 15);
+        if(ans){
+            System.out.println("Element is present in the linked list");
+        }
+        else{
+            System.out.println("Element is not present in the linked list");
+        }
+    }
+}
