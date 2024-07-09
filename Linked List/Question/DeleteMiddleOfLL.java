0c20adee039446ecf4fb13829f870e1e2a4eae0f
Navneet Singh <navneetsingh1825@gmail.com>
2024-07-09 19:29:56 +0530

    Initial commit

---
+++ Linked List/Question/DeleteMiddleOfLL.java
@@ -0,0 +1,114 @@
+/*
+L.C: 2095. Delete the Middle Node of a Linked List
+You are given the head of a linked list. Delete the middle node, and return the head of the modified linked list.
+
+The middle node of a linked list of size n is the ⌊n / 2⌋th node from the start using 0-based indexing, where ⌊x⌋ denotes the largest integer less than or equal to x.
+
+For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2, respectively.
+ 
+Example 1:
+Input: head = [1,3,4,7,1,2,6]
+Output: [1,3,4,1,2,6]
+Explanation:
+The above figure represents the given linked list. The indices of the nodes are written below.
+Since n = 7, node 3 with value 7 is the middle node, which is marked in red.
+We return the new list after removing this node. 
+
+Example 2:
+Input: head = [1,2,3,4]
+Output: [1,2,4]
+Explanation:
+The above figure represents the given linked list.
+For n = 4, node 2 with value 3 is the middle node, which is marked in red.
+Example 3:
+
+
+Input: head = [2,1]
+Output: [2]
+Explanation:
+The above figure represents the given linked list.
+For n = 2, node 1 with value 1 is the middle node, which is marked in red.
+Node 0 with value 2 is the only node remaining after removing node 1.
+ 
+Constraints:
+
+The number of nodes in the list is in the range [1, 105].
+1 <= Node.val <= 105
+ */
+class Node{
+    Node next;
+    int data;
+    Node(Node next,int data){
+        this.next = next;
+        this.data = data;
+    }
+    Node(int data){
+        this.next = null;
+        this.data = data;
+    }
+}
+public class DeleteMiddleOfLL {
+    public static Node convertToLL(int arr[]){
+        Node head = new Node(arr[0]);
+        Node mover = head;
+        for(int i =1;i<arr.length;i++){
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+        return head;
+    }
+    public static void printLL(Node head){
+        while(head != null){
+            System.out.print(head.data+" ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+    public static Node deleteNode(Node head){
+        if(head == null || head.next == null){
+            return null;
+        }
+        Node temp = head;
+        int count = 0;
+        while(temp != null){
+            count++;
+            temp = temp.next;
+        }
+        int res = count/2;
+        temp = head;
+        while(temp != null){
+            res--;
+            if(res == 0){
+                temp.next = temp.next.next;
+                break;
+            }
+            temp = temp.next;
+        }
+        return head;
+    }
+    // public static Node deleteNode(Node head){
+    //     if(head == null || head.next == null){
+    //         return null;
+    //     }
+    //     Node slow = head;
+    //     Node fast = head;
+    //     fast = fast.next.next;
+    //     while(fast != null && fast.next != null){
+    //         slow = slow.next;
+    //         fast = fast.next.next;
+    //     }
+    //     slow.next = slow.next.next;
+    //     return head;
+    // }
+    
+    public static void main(String args[]){
+        int arr[] = new int[]{1,2,3,4,5,6};
+        Node head = convertToLL(arr);
+        System.out.print("Linked list Before deletion of Middle Node of LL: ");
+        printLL(head);
+        head = deleteNode(head);
+        System.out.print("Linked list after deletion of Middle Node of LL: ");
+        printLL(head);
+    }
+}
