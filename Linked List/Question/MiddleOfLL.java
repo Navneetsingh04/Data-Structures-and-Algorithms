eddf7b55fabea6959903af69af3dbe558337abb8
Navneet Singh <navneetsingh1825@gmail.com>
2024-07-13 15:18:38 +0530

    Initial commit

---
+++ Linked List/Question/MiddleOfLL.java
@@ -0,0 +1,68 @@
+/*
+L.C: 876. Middle of the Linked List
+
+Given the head of a singly linked list, return the middle node of the linked list.
+If there are two middle nodes, return the second middle node.
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+
+Example 2:
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+Constraints:
+
+The number of nodes in the list is in the range [1, 100].
+1 <= Node.val <= 100
+ */
+class Node{
+    Node next;
+    int data;
+    Node(Node next,int data){
+        this.next = next;
+        this.data = data;
+    }
+    Node(int data){
+        this.next = null;
+        this.data = data;
+    }
+}
+public class MiddleOfLL {
+    public static Node convertToLL(int arr[]){
+        Node head = new Node(arr[0]);
+        Node mover = head;
+        for(int i =1;i<arr.length;i++){
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+        return head;
+    }
+    public static void printLL(Node head){
+        while(head != null){
+            System.out.print(head.data+" ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+    public static Node middleNode(Node head){
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    public static void main(String args[]){
+        int arr[] = new int[]{1,2,3,4,5,6};
+        Node head = convertToLL(arr);
+        
+        head = middleNode(head);
+        System.out.print("Middle of the LL is: ");
+        printLL(head);
+    }
+}
