e9ec58ce951a4611cbd2a10286a8d9df104eb477
Navneet Singh <navneetsingh1825@gmail.com>
2024-08-16 15:20:37 +0530

    Adding function  for deletion

--- ARRAYS/Array Operations/Deletion.java
+++ ARRAYS/Array Operations/Deletion.java
@@ -1,11 +1,22 @@
 import java.util.Scanner;
 public class Deletion {
+    public static void deleteElement(int arr[],int n,int position){
+        if(position < 1 || position > n){
+            System.out.println("Inavlid Position");
+        }
+        else{
+            for(int i = position;i<n-1;i++){
+                arr[i] = arr[i+1];
+            }
+            n--;
+        }
+    }
     public static void main(String args[]){
         Scanner s = new Scanner(System.in);
         System.out.print("Enter the size of array: ");
         int n = s.nextInt();
         int arr[] = new int[n];
-        System.out.println("Enter the Element array");
+        System.out.println("Enter the Element in the array");
         for(int i = 0;i<n;i++){
             arr[i] = s.nextInt();
         }
@@ -13,24 +24,13 @@ public class Deletion {
         for(int i: arr){
             System.out.print(i+" ");
         }
-        System.out.print("\nEnter the positon from which you want to delete an element: ");
+        System.out.print("\nEnter the positon to delete an element: ");
         int position = s.nextInt();
-
-        if(position<0 || position>=n){
-            System.out.println("Invalid position!");
-        }
-        else{
-            for(int i  = position;i<n-1;i++){
-                arr[i] = arr[i+1];
-            }
-            n--;
-            System.out.println("Element deleted Sucessfully!");
-        }
-        System.out.print("\nArray after deletion: ");
-        for(int i = 0;i<n;i++){
+        deleteElement(arr, n, position);
+        System.out.print("Array after deletion: ");
+        for(int i = 0;i<n-1;i++){
             System.out.print(arr[i]+" ");
         }
         s.close();
     }
-
 }
