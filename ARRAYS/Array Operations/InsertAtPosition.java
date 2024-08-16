c9ce3cdd74d23b457c50b5ec6f07816821ee4d8b
Navneet Singh <navneetsingh1825@gmail.com>
2024-08-16 20:02:20 +0530

    Initial commit

--- ARRAYS/Array Operations/InsertAtPosition.java
+++ ARRAYS/Array Operations/InsertAtPosition.java
@@ -1,11 +1,26 @@
 import java.util.Scanner;
 public class InsertAtPosition {
+    public static int[] insertElement(int arr[],int n,int key,int position){
+        if(position < 1 || position > n+1){
+            System.out.println("Invalid Position");
+            return arr;
+        }
+        int newArray[] = new int[n+1];
+        for(int i = 0;i<position-1;i++){
+            newArray[i] = arr[i];
+        }
+        newArray[position-1] = key;
+        for(int i = position;i<newArray.length;i++){
+            newArray[i] =  arr[i-1];
+        }
+        return newArray;
+    }
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.print("Enter the size of array: ");
         int n = s.nextInt();
         int arr[] = new int[n];
-        System.out.println("Enter the Element array");
+        System.out.print("Enter the Element in array: ");
         for(int i = 0;i<n;i++){
             arr[i] = s.nextInt();
         }
@@ -13,23 +28,15 @@ public class InsertAtPosition {
         for(int i: arr){
             System.out.print(i+" ");
         }
-        System.out.print("\nEnter the position you want to insert in the array: ");
+        System.out.print("\nEnter the position: ");
         int position = s.nextInt();
-        int newArray[] = new int[n+1];
         System.out.print("Enter the element to insert: ");
         int key = s.nextInt();
-        for(int i = 0;i<position;i++){
-            newArray[i] = arr[i];
-        }
-        newArray[position] = key;
-        for(int i = position+1;i<newArray.length;i++){
-            newArray[i] =  arr[i-1];
-        }
-        arr = newArray;
         System.out.print("Array After insertion: ");
-            for(int i : arr) {
-                System.out.print(i + " ");
-            }
+        arr = insertElement(arr, n, key, position);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
         s.close();
     }
 }
