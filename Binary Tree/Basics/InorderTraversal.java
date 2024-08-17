b0dc531cf81a6d94d150f902269652229503de25
Navneet Singh <navneetsingh1825@gmail.com>
2024-08-17 09:03:27 +0530

    Initial commit

--- Binary Tree/Basics/InorderTraversal.java
+++ Binary Tree/Basics/InorderTraversal.java
@@ -10,26 +10,21 @@ public class InorderTraversal {
             right = null;
         }
     }
-    public static Node createTree(){
+    public static void inorderTraversal(Node root){
+        if(root == null){
+            return;
+        }
+        inorderTraversal(root.left);
+        System.out.print(root.data+" ");
+        inorderTraversal(root.right);
+    }
+    public static void main(String args[]){
         Node root = new Node(1);
         root.left = new Node(2);
         root.right = new Node(3);
         root.left.left = new Node(4);
         root.left.right = new Node(5);
         root.right.right = new Node(6);
-
-        return root;
-    }
-    public static void inorderTraversal(Node root){
-        if(root == null){
-            return;
-        }
-        inorderTraversal(root.left);
-        System.out.print(root.data+"->");
-        inorderTraversal(root.right);
-    }
-    public static void main(String args[]){
-        Node root = createTree();
         System.out.print("Inorder Traversal: ");
         inorderTraversal(root);
     }
