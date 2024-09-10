93bf5c952b884067e1b87cb8a04dbc32820085ce
Navneet Singh <navneetsingh1825@gmail.com>
2024-09-11 01:31:04 +0530

    Initial commit

--- Bit Manipualtion/Basics/ResetBit.java
+++ Bit Manipualtion/Basics/ResetBit.java
@@ -10,12 +10,12 @@ public class ResetBit {
         return result;
     }
     public static void main(String args[]){
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter the number: ");
-        int num = s.nextInt();
-        System.out.print("Enter the the bit positon to set: ");
-        int pos = s.nextInt();
-        System.out.println("Number with bit at position "+pos+" Reset to 0: "+resetBit(num, pos));
-        s.close();
+        try (Scanner s = new Scanner(System.in)) {
+            System.out.print("Enter the number: ");
+            int num = s.nextInt();
+            System.out.print("Enter the the bit positon to set: ");
+            int pos = s.nextInt();
+            System.out.println("Number with bit at position "+pos+" Reset to 0: "+resetBit(num, pos));
+        }
     }
 }
