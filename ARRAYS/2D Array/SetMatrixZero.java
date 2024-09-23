7f8b579baf7f6ec7ea3df3b6473b17b971dcfc23
Navneet Singh <navneetsingh1825@gmail.com>
2024-09-24 00:49:39 +0530

    Initial commit

--- ARRAYS/2D Array/SetMatrixZero.java
+++ ARRAYS/2D Array/SetMatrixZero.java
@@ -48,9 +48,9 @@ public class SetMatrixZero {
     public static void main(String[] args) {
         int matrix[][] = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
         setZeroes(matrix);
-        for(int i = 0;i<matrix.length;i++){
-            for(int j = 0;j<matrix[0].length;j++){
-                System.out.print(matrix[i][j]+" ");
+        for(int[] matrix1 : matrix) {
+            for (int j = 0; j<matrix[0].length; j++) {
+                System.out.print(matrix1[j] + " ");
             }
             System.out.println();
         }
