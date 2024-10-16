98c33f7a66ffa22f87ee0ba3131b3d5e5db3d6f3
Navneet Singh <navneetsingh1825@gmail.com>
2024-10-16 10:33:19 +0530

    Initial commit

--- Sliding Window/LongestSubarryaSumEqualTok.java
+++ Sliding Window/LongestSubarryaSumEqualTok.java
@@ -1,5 +1,24 @@
 public class LongestSubarryaSumEqualTok {
+    public static int longestSubarray(int arr[],int k){
+        int n = arr.length;
+        int maxLen = 0;
+        for(int i = 0;i<n;i++){
+            int sum = 0;
+            for(int j = i;j<n;j++){
+                sum += arr[j];
+                if(sum <= k){
+                    maxLen = Math.max(maxLen,j-i+1);
+                }
+                else if(sum>k){
+                    break;
+                }
+            }
+        }
+        return maxLen;
+    }
     public static void main(String[] args) {
-        
+        int arr[] = {2,5,1,7,10};
+        int k = 13;
+        System.out.println(longestSubarray(arr, k));
     }
 }
