5ca55cc52e539419cdb144acfc8b604be168c671
Navneet Singh <navneetsingh1825@gmail.com>
2024-12-28 20:27:09 +0530

    Initial commit

--- Array/Array Basics Questions/SecondLargestElement.java
+++ Array/Array Basics Questions/SecondLargestElement.java
@@ -1,6 +1,12 @@
-// WAP to find and return the second largest element in an array of integers.
+/*
+Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then return -1. 
+*/
 public class SecondLargestElement {
     public static int secondLargest(int arr[]){
+        int n = arr.length;
+        if(arr == null || n < 2){
+            return -1;
+        }
         int max = arr[0];
         int secondMax = Integer.MIN_VALUE;
         for(int i = 0;i<arr.length;i++){
@@ -11,6 +17,9 @@ public class SecondLargestElement {
             else if (arr[i] > secondMax && arr[i] != max) {
                 secondMax = arr[i];
             }
+        }
+        if(secondMax == Integer.MIN_VALUE){
+            return -1;
         }
         return secondMax;
     }
