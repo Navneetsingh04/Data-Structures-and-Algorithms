5c4ee262b1d4d47363a5fb968eb748cd18188228
Navneet Singh <navneetsingh1825@gmail.com>
2025-03-26 23:52:22 +0530

    refactor Merge Sort implementation for improved readability and efficiency

--- Recursion/DnC/MerSort.java
+++ Recursion/DnC/MerSort.java
@@ -1,64 +1,39 @@
 public class MerSort{
-    public static void merg(int arr[],int start,int end,int mid){
-        int leftLen =  mid-start+1;
-        int rightLen = end-mid;
-        // creating left and right array
-        int left[] = new int[leftLen];
-        int right[] = new int[rightLen];
-        // copy values from  original array t0 left array
-        int k = start;
-        for(int i = 0;i<leftLen;i++){
-            left[i] = arr[k];
-            k++;
+    public static void merg(int arr[],int l,int mid,int r){
+        int n1 = mid-l+1;
+        int n2 = r-mid;
+        int leftArr[] = new int[n1];
+        int rightArr[] = new int[n2];
+        
+        for(int i = 0;i<n1;i++){
+            leftArr[i] = arr[l+i];
         }
-        // copy values from  original array to Right array
-        k = mid+1;
-        for(int i = 0;i<rightLen;i++){
-            right[i] = arr[k];
-            k++;
+        for(int i= 0;i<n2;i++){
+            rightArr[i] = arr[mid+1+i];
         }
-        // marging
-        int leftIndex = 0;
-        int rightIndex = 0;
-        int mainArrayIndex = start;
-
-        while(leftIndex < leftLen && rightIndex < rightLen){
-            if(left[leftIndex] < right[rightIndex]){
-                arr[mainArrayIndex] = left[leftIndex];
-                mainArrayIndex++;
-                leftIndex++;
+        int i = 0,j = 0,k = l;
+        while(i<n1 && j<n2){
+            if(leftArr[i] <= rightArr[j]){
+                arr[k++] = leftArr[i++];
             }
             else{
-                arr[mainArrayIndex] = right[rightIndex];
-                mainArrayIndex++;
-                rightIndex++;
+                arr[k++] = rightArr[j++];
             }
-        } 
-        // 1 case -> left array exhaust but right array have some element left
-        while(rightIndex < rightLen){
-            arr[mainArrayIndex] = right[rightIndex];
-            mainArrayIndex++;
-            rightIndex++;
         }
-
-        // 2 case -> Right array exhaust but left array have some element left
-        while(leftIndex < leftLen){
-            arr[mainArrayIndex] = left[leftIndex];
-            mainArrayIndex++;
-            leftIndex++;
+        while(i<n1){
+            arr[k++] = leftArr[i++];
+        }
+        while(j<n2){
+            arr[k++] = rightArr[j++];
         }
     }
-    public static void mergSort(int arr[],int start,int end){
-        if(start >= end){
-            return;
+    public static void mergSort(int arr[],int l,int r){
+        if(l<r){
+            int mid = l+(r-l)/2;
+            mergSort(arr,l,mid);
+            mergSort(arr,mid+1,r);
+            merg(arr,l,mid,r);
         }
-        int mid = start+(end-start)/2;
-        // recursive call for left array
-        mergSort(arr, start, mid);
-        // recursive call for right array
-        mergSort(arr, mid+1, end);
-        merg(arr,start,end,mid);
-
     }
     public static void main(String[] args) {
         int arr[] = new int[]{2,1,6,9,4,5};
