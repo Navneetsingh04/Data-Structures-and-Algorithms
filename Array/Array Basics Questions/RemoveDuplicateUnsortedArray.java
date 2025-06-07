37ab53775257abc6633d89dcc154967f09d152e3
Navneet Singh <navneetsingh1825@gmail.com>
2025-06-07 16:45:56 +0530

    Implement multiple approaches

--- Array/Array Basics Questions/RemoveDuplicateUnsortedArray.java
+++ Array/Array Basics Questions/RemoveDuplicateUnsortedArray.java
@@ -11,7 +11,13 @@ The removal of duplicates must be done in-place, i.e., you're not allowed to use
 The order of elements must be preserved.
 */
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 public class RemoveDuplicateUnsortedArray {
+    // Approach 1
+    // Time Complexity: O(nlogn)
+    // Space Complexity: O(1)
+
     public static int removeDuplicate(int arr[]){
         if(arr.length == 0){
             return 0;
@@ -26,13 +32,57 @@ public class RemoveDuplicateUnsortedArray {
         }
         return index;
     }
+    // Approach 2
+    // Time Complexity: O(n^2)
+    // Space Complexity: O(1)
+    public static int removeDuplicateI(int arr[]){
+        int n = arr.length;
+        if(n == 0) return 0;
+        int index = 0;
+
+        for(int i = 0;i<n;i++){
+            boolean isDuplicate = false;
+            for(int j = 0;j<i;j++){
+                if(arr[i] == arr[j]){
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            if(!isDuplicate){
+                arr[index++] = arr[i];
+            }
+        }
+        return index;
+    }
+
+    // Approach 3: using HashSet
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+
+    public static int removeDuplicateII(int arr[]){
+        int n = arr.length;
+        if(n == 0) return 0;
+        Set<Integer> set = new HashSet<>();
+
+        for(int i: arr){
+            set.add(i);
+        }
+        int index = 0;
+        for(Object i: set){
+            arr[index++] = (int)i;
+        }
+        return index;
+    }
+
     public static void main(String[] args) {
         int arr[] = new int[]{13,1,1,8,2,3,13,4,4,5,5,6,6,7,8};
         System.out.print("Array Before Removing duplictaes: ");
         for(int i: arr){
             System.out.print(i+" ");
         }
-        int newlength = removeDuplicate(arr);
+        // int newlength = removeDuplicate(arr);
+        // int newlength = removeDuplicateII(arr);
+        int newlength = removeDuplicateI(arr);
         System.out.print("\nArray after Removing duplictaes: ");
         for(int i = 0;i<newlength;i++){
             System.out.print(arr[i]+" ");
